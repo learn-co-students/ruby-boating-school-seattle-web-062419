017680bc5f6310cb196efa9b2efb4df2062b3ec3
aminamos <26092352+aminamos@users.noreply.github.com>
2019-07-06 14:16:33 -0700

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,38 @@
 class Student
 
+    attr_reader :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def self.find_student(name)
+        Student.all.find { |student| student.first_name == name }
+    end
+
+    def grade_percentage
+        tot_pass = 0.0
+        tot_fail = 0.0
+
+        BoatingTest.all.each do |test|
+            if test.student == self && test.test_status == "passed"
+                tot_pass += 1
+            else
+                tot_fail += 1
+            end
+        end
+
+        (tot_pass / tot_fail).round(2)
+    end
 end
